3862a6df23ea16b2a7a57ef4b0f0e9ef2673d405
romainwidmer <romwidmer@gmail.com>
2019-03-25 13:05:14 +0100

    feat: data validation

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
+ * @UniqueEntity("title")
  */
 class Property {
 
@@ -23,6 +26,7 @@ class Property {
     private $id;
 
     /**
+     * @Assert\Length(min=5, max=255)
      * @ORM\Column(type="string", length=255)
      */
     private $title;
@@ -33,6 +37,7 @@ class Property {
     private $description;
 
     /**
+     * @Assert\Range(min=10, max=400)
      * @ORM\Column(type="integer")
      */
     private $surface;
@@ -84,6 +89,7 @@ class Property {
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^[0-9]{4,5}$/")
      */
     private $zip_code;
 
